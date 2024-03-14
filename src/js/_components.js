3bc9bd8c88c41b7205ae2a0bd0d4deeecb9691ee
unknown <suppertoha@yandex.ru>
2024-03-14 10:37:43 +0300

    header and footer

---
+++ src/js/_components.js
@@ -0,0 +1,8 @@
+import './components/burger';
+import './components/accordion';
+import './components/show-more-menu';
+import './components/select';
+import './components/modal';
+import './components/jsCurrentYear';
+
+
